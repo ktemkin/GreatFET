6979f9afebaa7f812ec2401b1fddab2a3cda81c6
Kate J. Temkin <k@ktemkin.com>
2019-08-19 04:42:53 -0600

    host tools: add some documentation

--- host/greatfet/__init__.py
+++ host/greatfet/__init__.py
@@ -1,5 +1,40 @@
+""" tools for working with GreatFET boards from python
+
+To use these tools programmatically, usually you'll want to use the GreatFET()
+factory function, which will automatically connect to the first available GreatFET
+board. For example:
+
+    from greatfet import GreatFET
+    gf = GreatFET()
+
+You can also limit the boards connected to by serial number:
+
+    import greatfet
+    gf = greatfet.GreatFET(serial_number="000041a465d9344a2617")
+
+You can also get a list of all connected GreatFETs by using the find_all argument:
+
+    import greatfet
+    gf = greatfet.GreatFET(find_all=True)
+
+
+You can find more information on the features supported by the relevant GreatFET by calling
+help on one of your GreatFET objects:
+
+    gf = greatfet.GreatFET()
+    help(gf)
+
+You can also request help on properties of the individual GreatFET objects;
+
+    help(gf.leds[1])
+    help(gf.gpio)
+
+
+"""
+
 from __future__ import print_function
 # Alias objects to make them easier to import.
+
 from .greatfet import GreatFET
 GreatFET = GreatFET  # pyflakes
 
